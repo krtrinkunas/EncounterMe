89b6b11e03b87a1a2400f7e2fb570504b51676ab
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-09-14 21:37:21 +0300

    Add project files.

---
+++ EncounterMeApp/EncounterMeApp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using EncounterMeApp.Models;
+using EncounterMeApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EncounterMeApp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
