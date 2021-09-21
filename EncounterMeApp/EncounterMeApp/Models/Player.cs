30ce09c90d8726ffaa2dd076b1f6bdaf2a8ec754
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-09-21 13:16:15 +0300

    Create list of players in leaderboard

---
+++ EncounterMeApp/EncounterMeApp/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncounterMeApp.Models
+{
+    public class Player
+    {
+        public string NickName { get; set; }
+        public string Points { get; set; }
+        public string ProfilePic { get; set; }
+    }
+}
--- EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
+++ EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
@@ -1,33 +1,41 @@
-﻿using MvvmHelpers;
+﻿using EncounterMeApp.Models;
+using MvvmHelpers;
 using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace EncounterMeApp.ViewModels
 {
     public class LeaderboardViewModel : BaseViewModel //Imported from MVVM helpers
     {
+        public ObservableRangeCollection<Player> Player { get; set; }
+
+        public AsyncCommand RefreshCommand { get; }
         public LeaderboardViewModel()
         {
-            IncreaseCount = new Command(OnIncrease); //Imported from MVVM helpers
-            Title = "Leaderboad"; //From MVVM helper
-        }
-        public ICommand IncreaseCount { get; }
+            Title = "Leaderboard";
 
-        int count = 0;
-        string countDisplay = "Click Me";
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set => SetProperty(ref countDisplay, value); //From MVVM helper
+            Player = new ObservableRangeCollection<Player>();
+
+            var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
+
+            Player.Add(new Player { NickName = "Destroyer3000", Points = "2000 points", ProfilePic = image });
+            Player.Add(new Player { NickName = "Enjoyer3000", Points = "1000 points", ProfilePic = image });
+            Player.Add(new Player { NickName = "Dinosower3000", Points = "500 points", ProfilePic = image });
+
+            RefreshCommand = new AsyncCommand(Refresh);
         }
 
-        void OnIncrease()
+        async Task Refresh()
         {
-            count++;
-            CountDisplay = $"You clicked {count} time(s)";
+            IsBusy = true;
+
+            await Task.Delay(2000);
+
+            IsBusy = false;
         }
     }
 }
