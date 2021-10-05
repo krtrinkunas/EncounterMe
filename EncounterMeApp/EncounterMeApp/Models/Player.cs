c187f0c23e67c181a5cacf12e46c3f6db6ac103a
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-10-05 13:28:51 +0300

    Player class implements IComparable interface so it can be sorted by points

--- EncounterMeApp/EncounterMeApp/Models/Player.cs
+++ EncounterMeApp/EncounterMeApp/Models/Player.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace EncounterMeApp.Models
 {
-    public class Player
+    public class Player : IComparable
     {
         public enum PlayerType { Creator, Explorer};
         public string NickName { get; set; }
-        public string Points { get; set; }
+        public int Points { get; set; }
         public string ProfilePic { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Player))
+                throw new ArgumentException("Object is not a Player!");
+
+            Player other = (Player)obj;
+            return Points.CompareTo(other.Points);
+        }
     }
 }
--- EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
+++ EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
@@ -3,6 +3,7 @@ using MvvmHelpers;
 using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -11,26 +12,29 @@ namespace EncounterMeApp.ViewModels
 {
     public class LeaderboardViewModel : BaseViewModel //Imported from MVVM helpers
     {
-        public ObservableRangeCollection<Player> Player { get; set; }
+        public List<Player> Player { get; set; }
 
         public AsyncCommand RefreshCommand { get; }
         public LeaderboardViewModel()
         {
             Title = "Leaderboard";
 
-            Player = new ObservableRangeCollection<Player>();
+            Player = new List<Player>();
 
             var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
 
-            Player.Add(new Player { NickName = "Destroyer3000", Points = "2894 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "Enjoyer69420", Points = "468465 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "Dinosower", Points = "4881 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "GetRekt", Points = "89610 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "1336", Points = "8412 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "Ninja", Points = "21 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "Shroud", Points = "51127 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "PashaBiceps", Points = "819856 points", ProfilePic = image });
-            Player.Add(new Player { NickName = "NoScope", Points = "754 points", ProfilePic = image });
+            Player.Add(new Player { NickName = "Destroyer3000", Points = 2894, ProfilePic = image });
+            Player.Add(new Player { NickName = "Enjoyer69420", Points = 468465, ProfilePic = image });
+            Player.Add(new Player { NickName = "Dinosower", Points = 4881, ProfilePic = image });
+            Player.Add(new Player { NickName = "GetRekt", Points = 89610, ProfilePic = image });
+            Player.Add(new Player { NickName = "1336", Points = 8412, ProfilePic = image });
+            Player.Add(new Player { NickName = "Ninja", Points = 21, ProfilePic = image });
+            Player.Add(new Player { NickName = "Shroud", Points = 51127, ProfilePic = image });
+            Player.Add(new Player { NickName = "PashaBiceps", Points = 819856, ProfilePic = image });
+            Player.Add(new Player { NickName = "NoScope", Points = 754, ProfilePic = image });
+
+            Player.Sort();
+            Player.Reverse();
 
             RefreshCommand = new AsyncCommand(Refresh);
         }
