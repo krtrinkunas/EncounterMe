1bd8c168ee3118db42bf9ce67c43c260036ef039
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-10-05 16:11:51 +0300

    Implement Data Binding to Profile Page and create Profile Page ViewModel

---
+++ EncounterMeApp/EncounterMeApp/ViewModels/ProfilePageViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace EncounterMeApp.ViewModels
+{
+    public class ProfilePageViewModel //: INotifyPropertyChanged
+    {
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        /*
+        private string name = string.Empty;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name == value)
+                {
+                    return;
+                }
+
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        */
+
+        public string DisplayName => "TEST PEPA";
+        /*
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        */
+    }
+}
--- EncounterMeApp/EncounterMeApp/Views/ProfilePage.xaml.cs
+++ EncounterMeApp/EncounterMeApp/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EncounterMeApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,30 @@ namespace EncounterMeApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        public Player newPlayer;
+     
         public ProfilePage()
         {
             InitializeComponent();
+
+            var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
+            newPlayer = new Player { NickName = "Test nickname", LocationsOwned = 2, LocationsVisited = 11, Points = 4856, ProfilePic = image };
         }
+
+        /*
+        private string _testNick = "Test String";
+        public string TestNick
+        {
+            get => _testNick;
+            set
+            {
+                if (value != _testNick)
+                {
+                    _testNick = value;
+                    OnPropertyChanged(nameof(TestNick));
+                }
+            }
+        }
+        */
     }
 }
