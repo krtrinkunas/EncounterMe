3a88b2482763d2e3df79f4fdd7649bc0d1c556bf
PijusPale <90030227+PijusPale@users.noreply.github.com>
2021-10-07 15:38:16 +0300

    Implenent struct

--- EncounterMeApp/EncounterMeApp/Views/MapPage.xaml.cs
+++ EncounterMeApp/EncounterMeApp/Views/MapPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
@@ -14,6 +15,23 @@ namespace EncounterMeApp.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MapPage : ContentPage
     {
+        //private readonly Geocoder _geocoder = new Geocoder();
+        string file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "points.txt");
+        public struct Location
+        {
+            public Location(Position pos, int point, string name, string own = "No owner")
+            {
+                position = pos;
+                NAME = name;
+                points = point;
+                owner = own;
+            }
+
+            public Position position { get; }
+            public string NAME { get; }
+            public int points { get; }
+            public string owner { get; }
+        }
         public MapPage()
         {
             InitializeComponent();
@@ -23,24 +41,27 @@ namespace EncounterMeApp.Views
 
         public void DisplayExistingPins()
         {
+            Location a = new Location(new Position(54.684384, 25.277140), WriteReadFile(), "Petro Cvirkos aikštė");
             var pin1 = new Pin()
             {
-                Position = new Position (54.684084, 25.276133),
-                Label = "Petro Cvirkos aikštė"
+                Position = a.position,
+                Label = a.NAME
             };
+            pin1.MarkerClicked += async (s, args) =>
+            {
+                args.HideInfoWindow = true;
+                string pinName = ((Pin)s).Label;
+                await DisplayActionSheet(pinName, "Cancel", "Occupy", $"COORDS: {((Pin)s).Position.Latitude},{((Pin)s).Position.Longitude}",
+                    $"Points: {a.points}", $"Owner: {a.owner}");
+            };
+
             var pin2 = new Pin()
             {
                 Position = new Position(54.685372, 25.286621),
                 Label = "Katedra"
             };
             mapOfVilnius.Pins.Add(pin1);
-            pin1.MarkerClicked += Pin1_MarkerClicked;
             mapOfVilnius.Pins.Add(pin2);
-        }
-
-        private void Pin1_MarkerClicked(object sender, PinClickedEventArgs e)
-        {
-            DisplayActionSheet("Petro Cvirkos aikštė", "Cancel", null, "Owner","Occupy");
         }
 
         public async void DisplayCurrentLocation()
@@ -53,10 +74,21 @@ namespace EncounterMeApp.Views
         {
             DisplayAlert("You", "Clicked", "THE BUTTON");
         }
-
-        private void mapOfVilnius_MapClicked(object sender, MapClickedEventArgs e)
+        public int WriteReadFile()
         {
+            File.WriteAllText(file, "1234a");
+            string pointsStr = File.ReadAllText(file);
 
+            try
+            {
+                int result = Int32.Parse(pointsStr);
+                return result;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine($"Unable to parse '{pointsStr}'");
+                return 0;
+            }
         }
     }
 }
