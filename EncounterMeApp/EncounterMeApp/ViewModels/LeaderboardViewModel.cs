ba7d19690936314bfa37426c336d6b9e1e129d5c
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-10-07 22:59:14 +0300

    fix bugs in leaderboard page

--- EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
+++ EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
@@ -3,7 +3,6 @@ using MvvmHelpers;
 using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -21,7 +20,7 @@ namespace EncounterMeApp.ViewModels
 
             Player = new List<Player>();
 
-            var image = "https://e7.pngegg.com/pngimages/922/865/png-clipart-discord-pepe-the-frog-video-games-pepe-thumbnail.png";
+            var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
 
             Player.Add(new Player { NickName = "Destroyer3000", Points = 2894, ProfilePic = image });
             Player.Add(new Player { NickName = "Enjoyer69420", Points = 468465, ProfilePic = image });
