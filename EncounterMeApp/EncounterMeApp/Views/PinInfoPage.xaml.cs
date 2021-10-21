741de02ba126ca7a7af2c7087c83f411284da3ab
PijusPale <90030227+PijusPale@users.noreply.github.com>
2021-10-21 18:28:43 +0300

    early implementation of PinInfoPage

---
+++ EncounterMeApp/EncounterMeApp/Views/PinInfoPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EncounterMeApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PinInfoPage : ContentPage
+    {
+        public PinInfoPage(string name, string owner, int points)
+        {
+            InitializeComponent();
+            nameOfPin.Text = name;
+            ownerOfPin.Text = "Owner: " + owner;
+            pointsOfPin.Text = points.ToString();
+        }
+    }
+}
