74963196657d1a74f21f8f282719e4e478656850
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-11-02 14:48:49 +0200

    implement Internet Service

--- EncounterMeApp/EncounterMeApp/Services/InternetPlayerService.cs
+++ EncounterMeApp/EncounterMeApp/Services/InternetPlayerService.cs
@@ -28,5 +28,38 @@ namespace EncounterMeApp.Services
             var players = JsonConvert.DeserializeObject<IEnumerable<Player>>(json);
             return players;
         }
+
+        static Random random = new Random();
+        public static async Task AddPlayer(string nickName, int points)
+        {
+            var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
+            var player = new Player
+            {
+                NickName = nickName,
+                Points = points,
+                ProfilePic = image,
+                Id = random.Next(0, 10000)
+            };
+
+            var json = JsonConvert.SerializeObject(player);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("api/Player", content);
+
+            if(!response.IsSuccessStatusCode)
+            {
+
+            }
+        }
+
+        public static async Task RemovePlayer(int id)
+        {
+            var response = await client.DeleteAsync($"api/Player/{id}");
+            if(!response.IsSuccessStatusCode)
+            {
+
+            }
+        }
+
     }
 }
