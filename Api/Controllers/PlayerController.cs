a57bc72fbe6de9ea424524dc73cac0561d5ad80d
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-11-02 19:23:56 +0200

    Implement api

--- Api/Controllers/PlayerController.cs
+++ Api/Controllers/PlayerController.cs
@@ -1,15 +1,60 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+//using MyCoffeeApp.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Api.Controllers
+
+namespace MyCoffeeApp.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PlayerController : ControllerBase
+    public class CoffeeController : ControllerBase
     {
+
+        public static List<Player> Players { get; } = new List<Player>();
+        // GET: api/Player
+        [HttpGet]
+        public IEnumerable<Player> Get()
+        {
+            return Players;
+        }
+
+        // GET api/Player/5
+        [HttpGet("{id}")]
+        public Player Get(int id)
+        {
+            return Players.FirstOrDefault(c => c.Id == id);
+        }
+
+        // POST api/Player
+        [HttpPost]
+        public void Post([FromBody] Player value)
+        {
+            Players.Add(value);
+        }
+
+        // PUT api/Player/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Player value)
+        {
+            var coffee = Players.FirstOrDefault(c => c.Id == id);
+            if (coffee == null)
+                return;
+
+            coffee = value;
+        }
+
+        // DELETE api/<PlayerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var coffee = Players.FirstOrDefault(c => c.Id == id);
+            if (coffee == null)
+                return;
+
+            Players.Remove(coffee);
+        }
     }
 }
