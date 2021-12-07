45daa0ba48c847a3d6b9ba95ca76a0faa8c3a76f
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-12-07 15:50:43 +0200

    Tests for valid and invalid players

--- AppTests/PlayerManagerTests.cs
+++ AppTests/PlayerManagerTests.cs
@@ -6,6 +6,7 @@ using Moq;
 using System.Threading.Tasks;
 using EncounterMeApp;
 using EncounterMeApp.Services;
+using System.Collections.Generic;
 
 namespace AppTests
 {
@@ -21,14 +22,29 @@ namespace AppTests
         }
 
         [Fact]
-        //playerService nustatyti, ka grazina metodas AddPlayer
         public async Task validateUser_GetsValidPlayer_ReturnsPlayer()
         {
-            Player testPlayer = new Player { NickName = "TestNickName", Points = 0, Email = "email@email.com", Id = 52, LocationsOwned = 0, LocationsVisited = 0, ProfilePic = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png", Type = 0, Firstname = "TestFirstName", Lastname = "TestLatsName", Password = "password123" };
+            Player validPlayer = new Player { NickName = "TestNickName", Points = 0, Email = "email@email.com", Id = 52, LocationsOwned = 0, LocationsVisited = 0, ProfilePic = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png", Type = 0, Firstname = "TestFirstName", Lastname = "TestLatsName", Password = "password123" };
 
-            Player validatedPlayer = await _sut.validateUser(testPlayer);
+            Player validatedPlayer = await _sut.validateUser(validPlayer);
 
             Assert.NotNull(validatedPlayer);
+        }
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public async Task validateUser_GetsInvalidPlayer_ReturnsNull(Player expected, Player player)
+        {
+            Player validatedPlayer = await _sut.validateUser(player);
+
+            Assert.Equal(expected, validatedPlayer);
+        }
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { null, new Player { NickName = "" } };
+            yield return new object[] { null, new Player { Firstname = "" } };
+            yield return new object[] { null, new Player { Lastname = "" } };
+            yield return new object[] { null, new Player { Email = "" } };
+            yield return new object[] { null, new Player { Password = "" } };
         }
     }
 }
