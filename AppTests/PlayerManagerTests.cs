bd5580b433c7139f18df6d8a05a276723401f000
Dominic-dul <dominykas.dulevicius@mif.stud.vu.lt>
2021-12-07 15:35:43 +0200

    Gerenalize testing

--- AppTests/PlayerManagerTests.cs
+++ AppTests/PlayerManagerTests.cs
@@ -13,15 +13,20 @@ namespace AppTests
     {
         Mock<IPlayerService> playerService = new();
 
+        private readonly PlayerManager _sut;
+
+        public PlayerManagerTests()
+        {
+            _sut = new PlayerManager(playerService.Object);
+        }
+
         [Fact]
         //playerService nustatyti, ka grazina metodas AddPlayer
         public async Task validateUser_GetsValidPlayer_ReturnsPlayer()
         {
             Player testPlayer = new Player { NickName = "TestNickName", Points = 0, Email = "email@email.com", Id = 52, LocationsOwned = 0, LocationsVisited = 0, ProfilePic = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png", Type = 0, Firstname = "TestFirstName", Lastname = "TestLatsName", Password = "password123" };
 
-            PlayerManager testPlayerValidation = new PlayerManager(playerService.Object);
-
-            Player validatedPlayer = await testPlayerValidation.validateUser(testPlayer);
+            Player validatedPlayer = await _sut.validateUser(testPlayer);
 
             Assert.NotNull(validatedPlayer);
         }
