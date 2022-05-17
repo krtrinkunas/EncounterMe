249c6444422c5d10ad536f3527421cbb7d7080e1
Emilija Svirskytė <86317595+EmilijaSvirsk@users.noreply.github.com>
2022-05-17 20:07:20 +0300

    added Friend section to Leaderboard

--- EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
+++ EncounterMeApp/EncounterMeApp/ViewModels/LeaderboardViewModel.cs
@@ -14,16 +14,22 @@ namespace EncounterMeApp.ViewModels
 {
     public class LeaderboardViewModel : BaseViewModel
     {
-        public ObservableRangeCollection<Player> Player { get; set; }
+        public ObservableRangeCollection<Player> Player1 { get; set; }
+        public ObservableRangeCollection<Player> PlayerFriends { get; set;}
+
+        Player currPlayer;
 
         IPlayerService playerService;
+        IFriendService friendService;
 
         public AsyncCommand RefreshCommand { get; }
         public LeaderboardViewModel()
         {
             Title = "Leaderboard";
+            this.currPlayer = App.player;
 
-            Player = new ObservableRangeCollection<Player>();
+            Player1 = new ObservableRangeCollection<Player>();
+            PlayerFriends = new ObservableRangeCollection<Player>();
 
             _ = Refresh();
             //var image = "https://cdn3.iconfinder.com/data/icons/games-11/24/_user-512.png";
@@ -31,6 +37,9 @@ namespace EncounterMeApp.ViewModels
             RefreshCommand = new AsyncCommand(Refresh);
 
             playerService = DependencyService.Get<IPlayerService>();
+            friendService = DependencyService.Get<IFriendService>();
+
+            
         }
 
         async Task Refresh()
@@ -39,12 +48,26 @@ namespace EncounterMeApp.ViewModels
 
             await Task.Delay(2000);
 
-            Player.Clear();
+            Player1.Clear();
+            PlayerFriends.Clear();
 
             var players = await playerService.GetPlayers();
+            var friendList = await friendService.GetFriends();
 
-            Player.AddRange(players);
-            Player.Sort((a, b) => { return a.CompareTo(b); });
+            foreach (var plr in friendList)
+            {
+                if (plr.Friend1ID == currPlayer.Id)
+                    PlayerFriends.Add(await playerService.GetPlayer(plr.Friend2ID));
+                else if (plr.Friend2ID == currPlayer.Id)
+                    PlayerFriends.Add(await playerService.GetPlayer(plr.Friend1ID));
+            }
+
+            Player1.AddRange(players);
+            Player1.Sort((a, b) => { return a.CompareTo(b); });
+
+            //PlayerFriends.AddRange(players);
+            PlayerFriends.Sort((a, b) => { return a.CompareTo(b); });
+            //PlayerFriends.Sort((a) => {return a}) //((a, b) => { return a.CompareTo(b); });
 
             IsBusy = false;
         }
